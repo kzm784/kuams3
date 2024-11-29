8aa4556a9275aa6d9d77e02d01ff66a763202bc8
kazuma yagi <kazulab10969@outlook.jp>
2024-11-29 15:48:56 +0900

    first commit

---
+++ kuams3_bringup/launch/rviz.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
+from launch.substitutions import Command
+import os
+from ament_index_python.packages import get_package_share_path
+
+def generate_launch_description():
+
+    rviz_config_path = os.path.join(get_package_share_path('kuams3_bringup'), 'rviz', 'kuams3.rviz')
+
+    rviz2_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=['-d', rviz_config_path]
+    )
+
+    return LaunchDescription([
+        rviz2_node
+    ])
