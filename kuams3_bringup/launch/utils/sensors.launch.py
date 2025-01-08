f19ff5638d8e0683f761d413d2cb9cb3021f2dca
kazuma yagi <kazulab10969@outlook.jp>
2025-01-08 16:56:28 +0900

    change mid-360 launch

--- kuams3_bringup/launch/utils/sensors.launch.py
+++ kuams3_bringup/launch/utils/sensors.launch.py
@@ -8,14 +8,33 @@ from ament_index_python.packages import get_package_share_directory
 import yaml
 
 def generate_launch_description():
-    # Include rviz_MID660_launch.py from livox_ros_driver2
-    livox_driver_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('livox_ros_driver2'), 'launch_ROS2', 'rviz_MID360_launch.py')
-        ),
+
+    bringup_dir = get_package_share_directory("kuams3_bringup")
+
+    # livox_ros_driver2 Node
+    livox_ros_driver2_node = Node(
+        package='livox_ros_driver2',
+        executable='livox_ros_driver2_node',
+        name='livox_ros_driver2',
+        output='screen',
+        parameters=[
+            {
+                'xfer_format': 0,
+                'multi_topic': 0,
+                'data_src': 0,
+                'publish_freq': 10.0,
+                'output_data_type': 0,
+                'frame_id': 'livox_frame',
+                'lvx_file_path': '',
+                'user_config_path': os.path.join(
+                    bringup_dir, 'config', 'MID360_config.json'
+                ),
+                'cmdline_input_bd_code': 'livox0000000001',
+            }
+        ]
     )
 
     
     return LaunchDescription([
-        livox_driver_launch
+        livox_ros_driver2_node
     ])
