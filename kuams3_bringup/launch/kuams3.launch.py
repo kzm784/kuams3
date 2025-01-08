304c497d12bf4ae496b0500b6161af2716f10ac9
kazuma yagi <kazulab10969@outlook.jp>
2025-01-08 16:56:55 +0900

    edit micro_ros device name

--- kuams3_bringup/launch/kuams3.launch.py
+++ kuams3_bringup/launch/kuams3.launch.py
@@ -1,6 +1,6 @@
 import os
 from ament_index_python.packages import get_package_share_path
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
 from launch import LaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
@@ -14,11 +14,24 @@ def generate_launch_description():
     description_package_path = get_package_share_path('kuams3_description')
     default_model_path = description_package_path / 'urdf/kuams3.xacro'
 
-    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
-                                      description='Absolute path to robot urdf file')
+    micro_ros_agent_node = Node(
+        package='micro_ros_agent',
+        executable='micro_ros_agent',
+        name='micro_ros_agent',
+        output='screen',
+        arguments=['serial', '--dev', '/dev/mecanumrover3', '-b', '115200']
+    )
 
-    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
-                                       value_type=str)
+    model_arg = DeclareLaunchArgument(
+        name='model',
+        default_value=str(default_model_path),
+        description='Absolute path to robot urdf file'
+    )
+
+    robot_description = ParameterValue(
+        Command(['xacro ', LaunchConfiguration('model')]),
+        value_type=str
+    )
 
     joint_state_publisher_node = Node(
         package='joint_state_publisher',
@@ -38,18 +51,24 @@ def generate_launch_description():
         parameters=[pub_odom_config_path]
     )
 
-    kuams_bringup_dir = get_package_share_path('kuams3_bringup')
-    sensors_launch_file = os.path.join(kuams_bringup_dir, 'launch', 'utils', 'sensors.launch.py')
-
+    sensors_launch_file = os.path.join(bringup_package_path, 'launch', 'utils', 'sensors.launch.py')
     include_sensors_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(sensors_launch_file)
     )
 
+    dependent_nodes = TimerAction(
+        period=5.0,
+        actions=[
+            joint_state_publisher_node,
+            robot_state_publisher_node,
+            pub_odom_node,
+            include_sensors_launch,
+        ]
+    )
+
     ld = LaunchDescription()
+    ld.add_action(micro_ros_agent_node)
     ld.add_action(model_arg)
-    ld.add_action(joint_state_publisher_node)
-    ld.add_action(robot_state_publisher_node)
-    ld.add_action(pub_odom_node)
-    ld.add_action(include_sensors_launch)
+    ld.add_action(dependent_nodes)
 
     return ld
